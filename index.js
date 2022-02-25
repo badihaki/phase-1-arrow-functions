bbb1b1ed38889978fede5a05a8f8efe8e28db2a9
Badi-Haki Martin <jojackblack@gmail.com>
2022-02-25 10:33:29 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+// making funct expression
+const divide = function(){
+    // doesn't need parameters
+    return 2000/100;
+}
+
+// arrow funct's
+const square = inputNumber => inputNumber*inputNumber;
+const add = (number1, number2) => number1 + number2;
